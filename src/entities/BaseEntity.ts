92eceb69766e8e7a623fe07e55528db0b677ba6d
aaron-dev <magnumcastle@icloud.com>
2024-03-20 19:49:51 +0800

    add base abstract class

---
+++ src/entities/BaseEntity.ts
@@ -0,0 +1,33 @@
+/**
+ * @CreateTime: 2024/03/20 18:58
+ * @Project: blog
+ * @Author: aaroncastle
+ * @GitHub: https://github.com/aaroncastle/blog
+ */
+import "reflect-metadata"
+import { validate } from "class-validator"
+import { ClassConstructor, plainToInstance } from "class-transformer"
+
+export abstract class BaseEntity {
+    public async validateSelf(skipMissingProperties = false): Promise<string[]> {
+        const errors = await validate(this, {
+            skipMissingProperties,
+        })
+        const result: string[] = []
+        // @ts-ignore
+        const temp = errors.map(item => Object.values(item.constraints))
+        temp.forEach(item => {
+            result.push(...item)
+        })
+        return result
+    }
+
+    protected static transform<T>(cls: ClassConstructor<T>, obj: object): T {
+        if (obj instanceof cls) {
+            return obj
+        } else {
+            return plainToInstance(cls, obj)
+        }
+    }
+}
+
