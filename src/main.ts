001dfab779319bbc7d375a76ab0e3838a10e7ae7
aaron-dev <magnumcastle@icloud.com>
2024-03-22 17:34:08 +0800

    create constructor for permission that will be used for magnager blog users

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,10 @@
  * @Author: aaroncastle
  * @GitHub: https://github.com/aaroncastle/blog
  */
-import { register } from "node:module";
-import { pathToFileURL } from "node:url";
-register("ts-node/esm", pathToFileURL("./"));
-function test(a:number,b:number):number{
-    return a + b;
-}
 
-console.log(test(1,20))
+import {createRequire} from "module"
+const json = createRequire(import.meta.url);
+const file = json('../config/config.json')
+console.log(file)
+
+
