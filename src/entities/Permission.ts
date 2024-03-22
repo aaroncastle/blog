702ec0f8f8e871831d91cee2dd400a1bed65f508
aaron-dev <magnumcastle@icloud.com>
2024-03-22 08:55:10 +0800

    init config.json

---
+++ src/entities/Permission.ts
@@ -0,0 +1,8 @@
+/** 
+ * @CreateTime: 2024/03/21 17:16 
+ * @Project: blog 
+ * @Author: aaroncastle
+ * @Github: https://github.com/aaroncastle/blog
+ * @Gitee: https://gitee.com/aaroncastle/blog
+ */
+$END$
