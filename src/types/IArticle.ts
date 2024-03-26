31dda54525052f19b0f1348927e39b659a0f2419
aaron-dev <magnumcastle@icloud.com>
2024-03-26 17:53:46 +0800

    add db.basename

---
+++ src/types/IArticle.ts
@@ -0,0 +1,8 @@
+/** 
+ * @CreateTime: 2024/03/22 17:50 
+ * @Project: blog 
+ * @Author: aaroncastle
+ * @Github: https://github.com/aaroncastle/blog
+ * @Gitee: https://gitee.com/aaroncastle/blog
+ */
+$END$
