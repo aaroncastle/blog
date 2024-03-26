ca7fb1d4edd0bd2485fc8f20cca5e759e780c6cd
aaron-dev <magnumcastle@icloud.com>
2024-03-26 17:55:01 +0800

    add articleSchema

--- src/db/articleSchema.ts
+++ src/db/articleSchema.ts
@@ -1,8 +1,33 @@
-/** 
- * @CreateTime: 2024/03/22 17:49 
- * @Project: blog 
+/**
+ * @CreateTime: 2024/03/22 17:49
+ * @Project: blog
  * @Author: aaroncastle
  * @Github: https://github.com/aaroncastle/blog
  * @Gitee: https://gitee.com/aaroncastle/blog
  */
-$END$
+import { model, plugin, Schema, set } from "mongoose"
+import { IArticle } from "../types/IArticle.js";
+import mongooseParanoidPlugin from "mongoose-paranoid-plugin"
+plugin(mongooseParanoidPlugin, { field: "deletedAt" })
+set("strictQuery", true)
+const blogSchema = new Schema<IArticle>(
+    {
+        title: String,
+        content: String,
+        views: {
+            type: Number,
+            default: 0,
+        },
+        toc: [Object],
+        picture: String,
+    },
+    {
+        versionKey: false,
+        collection: "article",
+        timestamps: true,
+        // @ts-ignore
+        paranoid: true,
+    }
+)
+
+export const ArticleModel = model<IArticle>("article", blogSchema)
